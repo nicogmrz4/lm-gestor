0b3b8eff9a7ee088e8dcdd3f78768854eddf93b7
Nicolas <nicolasgalarza40@gmail.com>
2024-05-06 23:34:22 -0300

    Add login feature

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\LoginType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(
+        Request $request, 
+        UserRepository $userRepository, 
+        UserPasswordHasherInterface $passwordHasher, 
+        Security $security): Response
+    {
+        $user = $this->getUser();
+
+        $loginForm = $this->createForm(LoginType::class, null, [
+            'action' => $this->generateUrl('app_login'),
+            'method' => 'POST',
+        ]);
+
+        $loginFormErrors = '';
+
+        $loginForm->handleRequest($request);
+        
+        if ($loginForm->isSubmitted() && $loginForm->isValid()) {
+            $userIdentifier = $loginForm->getData()['username'];
+            $user = $userRepository->findByEmailOrUsername($userIdentifier);
+            $plaintextPassword = $loginForm->getData()['password'];
+
+            if (is_null($user) || !$passwordHasher->isPasswordValid($user, $plaintextPassword)) {
+                $loginForm->addError(new FormError('Credenciales incorrectas'));
+            } else return $security->login($user); 
+        }
+
+        return $this->render('login/index.html.twig', [
+            'loginForm' => $loginForm,
+            'loginFormErrors' => $loginFormErrors
+        ]);
+    }
+}
---
+++ src/Form/LoginType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, ['label' => 'Usuario o email', 'help' => 'Ingrese su usuario o dirección electronico'])
+            ->add('password', PasswordType::class, ['label' => 'Contraseña'])
+            ->add('submit', SubmitType::class, ['label' => 'Iniciar Sesión'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
